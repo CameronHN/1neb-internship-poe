0857fa162a7773bfae49f6ad68bfea3d583be0ad
Cameron Noemdo <cameron@nebula.co.za>
2025-07-04 16:35:00 +0200

    Add interface for repository methods

---
+++ Core/Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Portfolio.Core.Entities;
+
+namespace Portfolio.Core.Contracts.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllUsers();
+
+        Task<User> CreateUser(User user);
+
+        Task<User> GetUserById(int Id);
+
+        Task<User> UpdateUser(User user);
+
+        Task<bool> DeleteUserById(int Id);
+    }
+}
