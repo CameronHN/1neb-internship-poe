3433ff3f6449b911200ddf52ccfbd5c4c2e1c27c
Cameron Noemdo <cameron@nebula.co.za>
2025-07-04 16:47:10 +0200

    Add repository file for Users

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Portfolio.Core.Contracts.Repositories;
+using Portfolio.Core.Entities;
+using Portfolio.Core.Exceptions;
+
+namespace Portfolio.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllUsers()
+        {
+            throw new NotFoundException("No users found");
+        }
+
+        public Task<User> GetUserById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
