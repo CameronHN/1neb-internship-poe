c79b7dea23498f61ba8e548a18a8ec0e74317b41
Cameron Noemdo <cameron@nebula.co.za>
2025-07-04 16:34:47 +0200

    Add some custom exceptions

---
+++ Core/Exceptions/CustomExceptions.cs
@@ -0,0 +1,27 @@
+﻿namespace Portfolio.Core.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message) { }
+    }
+
+    public class UnauthorizedAccessAppException : Exception
+    {
+        public UnauthorizedAccessAppException(string message) : base(message) { }
+    }
+
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message) { }
+    }
+
+    public class BusinessRuleViolationException : Exception
+    {
+        public BusinessRuleViolationException(string message) : base(message) { }
+    }
+}
