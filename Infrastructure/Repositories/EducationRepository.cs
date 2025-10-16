b94ebe2b9a2cca7a881ffc24706df39edace5f94
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-16 13:08:32 +0200

    Add base repos for education and skill

---
+++ Infrastructure/Repositories/EducationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Portfolio.Core.Contracts.Repositories;
+using Portfolio.Core.DTOs;
+
+namespace Portfolio.Infrastructure.Repositories
+{
+    public class EducationRepository : IEducationRepository
+    {
+        public Task<List<EducationItem>> GetAllEducationsByIds(List<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
