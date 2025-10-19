420fb1e338b2dee72e0cc79c786e5c4b813fa141
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-19 16:33:40 +0200

    Update request

--- Core/DTOs/ItemListRequest.cs
+++ Core/DTOs/ItemListRequest.cs
@@ -1,9 +1,15 @@
 ﻿namespace Portfolio.Core.DTOs
 {
+    public enum SortOrder
+    {
+        None,
+        Ascending,
+        Descending
+    }
+
     public class ItemListRequest
     {
         public List<Guid> Ids { get; set; } = [];
-        public bool IsDescending { get; set; } = false;
-
+        public SortOrder Order { get; set; } = SortOrder.None;
     }
 }
