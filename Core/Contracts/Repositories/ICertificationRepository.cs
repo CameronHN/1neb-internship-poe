88e6d6987f4d8fca5106101c256c94ac0b8bfe50
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-20 15:18:07 +0200

    Add base repository methods for certifications

--- Core/Contracts/Repositories/ICertificationRepository.cs
+++ Core/Contracts/Repositories/ICertificationRepository.cs
@@ -1,9 +1,22 @@
 ﻿using Portfolio.Core.DTOs;
+using Portfolio.Core.DTOs.Certification;
 
 namespace Portfolio.Core.Contracts.Repositories
 {
     public interface ICertificationRepository
     {
-        Task<List<CertificationItem>> GetAllCertsByIds(ItemListRequest request);
+        // Create
+        Task AddCertificationAsync(Guid userId, AddCertification certification);
+
+        // Read
+        Task<CertificationItem?> GetCertificationByIdAsync(Guid id);
+
+        // Update
+        Task UpdateCertificationAsync(Guid userId, UpdateCertification certification);
+
+        // Delete
+        Task DeleteCertificationAsync(Guid id);
+
+        Task<List<CertificationItem>> GetAllCertificationsByTheirIdsAsync(ItemListRequest request);
     }
 }
