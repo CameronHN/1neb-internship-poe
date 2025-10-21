0e52ae9ca8c0561b5fe06536a6109bd241f85864
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-21 16:07:30 +0200

    Add claims extensions to use for controllers

---
+++ Portfolio/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace Portfolio.WebApi.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid? GetUserId(this ClaimsPrincipal user)
+        {
+            var userIdClaim = user.FindFirstValue(ClaimTypes.NameIdentifier);
+            return Guid.TryParse(userIdClaim, out var userId) ? userId : null;
+        }
+
+        public static string? GetUserEmail(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
