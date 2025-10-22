18005dd7d75f797a9205e1a6b4eabfacaf660f72
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-22 09:19:54 +0200

    Add userID and csharpier

--- Core/DTOs/Certification/AddCertification.cs
+++ Core/DTOs/Certification/AddCertification.cs
@@ -4,33 +4,23 @@ namespace Portfolio.Core.DTOs.Certification
 {
     public class AddCertification
     {
-        [Required]
-        public Guid UserId { get; set; }
-
         [Required(ErrorMessage = "Certification name is required.")]
-        [MaxLength(
-            100,
-            ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage
-        )]
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
         [RegularExpression(@"\S.*", ErrorMessage = "Certification name cannot be whitespace.")]
         public required string CertificationName { get; set; }
 
-        [MaxLength(
-            100,
-            ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage
-        )]
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
         public string? IssuingOrganisation { get; set; }
 
         [Url(ErrorMessage = "Invalid URL format.")]
-        [MaxLength(
-            100,
-            ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage
-        )]
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
         public string? CredentialUrl { get; set; }
 
         public string? IssuedDate { get; set; }
 
         public string? ExpiryDate { get; set; }
 
+        [Required]
+        public Guid UserId { get; set; }
     }
 }
