063c7f79bf3b78bc402bd68385c5220338a8ea0f
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-22 09:20:48 +0200

    Add dtos for adding and updating educations

---
+++ Core/DTOs/Education/AddEducation.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Core.DTOs.Education
+{
+    public class AddEducation
+    {
+        [Required(ErrorMessage = "Institution name is required.")]
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
+        [RegularExpression(@"\S.*", ErrorMessage = "Institution name cannot be whitespace.")]
+        public required string InstitutionName { get; set; }
+
+        [Required(ErrorMessage = "Qualification is required.")]
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
+        [RegularExpression(@"\S.*", ErrorMessage = "Qualification cannot be whitespace.")]
+        public required string Qualification { get; set; }
+
+        public string? StartDate { get; set; }
+
+        public string? EndDate { get; set; }
+
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
+        public string? Major { get; set; }
+
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
+        public string? Achievement { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Core/DTOs/Education/UpdateEducation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.DTOs.Education
+{
+    public class UpdateEducation : AddEducation
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
