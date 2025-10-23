e5ae6e973b959686520b22761f943b8c64886445
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-23 15:43:12 +0200

    Add service implementation

---
+++ Application/Services/ProfessionalSummaryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Portfolio.Core.Contracts.Repositories;
+using Portfolio.Core.Contracts.Services;
+using Portfolio.Core.DTOs.ProfessionalSummary;
+
+namespace Portfolio.Application.Services
+{
+    public class ProfessionalSummaryService : IProfessionalSummaryService
+    {
+        private readonly IProfessionalSummaryRepository _professionalSummaryRepository;
+
+        public ProfessionalSummaryService(IProfessionalSummaryRepository professionalSummaryRepository)
+        {
+            _professionalSummaryRepository = professionalSummaryRepository;
+        }
+
+        public async Task<Guid> AddSummaryAsync(AddSummary summary)
+        {
+            return await _professionalSummaryRepository.AddSummaryAsync(summary);
+        }
+    }
+}
