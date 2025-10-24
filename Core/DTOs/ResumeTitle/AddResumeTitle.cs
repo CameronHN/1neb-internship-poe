10ae0c8909195f270717a9c87d5b88294a459c0e
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-24 15:57:38 +0200

    Add upsert DTOs for resume title

---
+++ Core/DTOs/ResumeTitle/AddResumeTitle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Portfolio.Core.DTOs.ResumeTitle
+{
+    public class AddResumeTitle
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(100, ErrorMessage = Constants.Constants.MaxCharacterLengthErrorMessage)]
+        [RegularExpression(@"\S.*", ErrorMessage = "Title cannot be whitespace.")]
+        public required string Title { get; set; }
+
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Core/DTOs/ResumeTitle/UpdateResumeTitle.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Core.DTOs.ResumeTitle
+{
+    public class UpdateResumeTitle : AddResumeTitle
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
