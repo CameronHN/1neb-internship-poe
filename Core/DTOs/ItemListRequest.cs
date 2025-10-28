f01904a2e7dff4adab26f931cf1d66081859a7bb
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-28 12:22:50 +0200

    Add summary for request class

--- Core/DTOs/ItemListRequest.cs
+++ Core/DTOs/ItemListRequest.cs
@@ -2,11 +2,21 @@
 {
     public enum SortOrder
     {
-        None,
-        Ascending,
-        Descending
+        None = 0,
+        Ascending = 1,
+        Descending = 2,
     }
 
+    /// <summary>
+    ///   <para>Defines a request model for retrieving a list of items by their unique IDs with optional sorting order.</para>
+    ///   <para>Includes a SortOrder enum for specifying sorting direction. Default order returns the list in the order the IDs were provided.</para>
+    ///   <para>
+    ///   Directions:
+    ///   <br/>None (default) = 0
+    ///   <br/>Ascending = 1
+    ///   <br/>Descending = 2
+    ///   </para>
+    /// </summary>
     public class ItemListRequest
     {
         public List<Guid> Ids { get; set; } = [];
