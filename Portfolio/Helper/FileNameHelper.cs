484f28581329e6160d281d7943e5569074a32a01
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-29 11:13:59 +0200

    Add summary to file name formatter helper

--- Portfolio/Helper/FileNameHelper.cs
+++ Portfolio/Helper/FileNameHelper.cs
@@ -2,11 +2,14 @@
 {
     public class FileNameHelper
     {
-        public static string FileNameFormatter(string username)
+        /// <summary>
+        /// Formats a file name for a resume based on the provided name.
+        /// </summary>
+        /// <returns>A formatted file name replacing whitespaces with underscores, appending "_resume". E.g. "John_Doe_resume".
+        /// If provided name is null or empty, returns "resume".</returns>
+        public static string FileNameFormatter(string name)
         {
-            return !string.IsNullOrEmpty(username)
-                ? username.Replace(' ', '_') + "_resume"
-                : "resume";
+            return !string.IsNullOrEmpty(name) ? name.Replace(' ', '_') + "_resume" : "resume";
         }
     }
 }
