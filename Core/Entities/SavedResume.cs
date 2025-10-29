1246b0e3e9b99aa1eb83042ac1e058d745267448
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-29 10:01:12 +0200

    Add entity to save user data

---
+++ Core/Entities/SavedResume.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Entities
+{
+    public class SavedResume
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "varchar(200)")]
+        public required string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public required string Data { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public required string TemplateType { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+    }
+}
