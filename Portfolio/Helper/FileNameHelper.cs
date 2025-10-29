6858d0d36d44819df544f27307e9490c1e1b8837
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-29 12:47:47 +0200

    Update param to nullable

--- Portfolio/Helper/FileNameHelper.cs
+++ Portfolio/Helper/FileNameHelper.cs
@@ -7,7 +7,7 @@
         /// </summary>
         /// <returns>A formatted file name replacing whitespaces with underscores, appending "_resume". E.g. "John_Doe_resume".
         /// If provided name is null or empty, returns "resume".</returns>
-        public static string FileNameFormatter(string name)
+        public static string FileNameFormatter(string? name)
         {
             return !string.IsNullOrEmpty(name) ? name.Replace(' ', '_') + "_resume" : "resume";
         }
